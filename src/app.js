8b81d3a22412bda1f4c22c202627dabc32bdaf2c
NOMAN MAJEED <nomanmajeed986@gmail.com>
2021-08-18 11:38:14 +0500

    Netflix Jumbotron Developed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import React from "react";
+import { JumbotronContainer } from "./containers/jumbotron";
+
 function App() {
   return (
-    "Hello"
+    <>
+      <JumbotronContainer />
+    </>
   );
 }
 
