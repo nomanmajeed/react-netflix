506a60754d2a0c6cce975301e2e92d26c9062162
NOMAN MAJEED <nomanmajeed986@gmail.com>
2021-08-18 11:52:56 +0500

    Netflix Footer Developed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
+import { FooterContainer } from "./containers/footer";
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FooterContainer />
     </>
   );
 }
